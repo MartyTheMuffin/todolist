a8f6d15c41ce8db38f9fce6620329b2162acc39a
Martin Krechetov <mkrechetov6700@gmail.com>
2023-12-06 14:32:36 +0200

    add files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const todoRouter = require('./routes/todo.routes');
+const dotenv = require('dotenv');
+const app = express();
+
+app.use(express.json());
+
+
+app.use('/', todoRouter);
+
+app.listen(3000, () => {
+    console.log('Server is running on Port 3000.');
+});
